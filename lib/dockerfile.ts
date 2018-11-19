822e11c892865d37583f8fa3c15be7495ffb338f
Cameron Diver <cameron@balena.io>
2018-11-19 18:15:28 +0000

    Run prettier on previously implemented modules
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- lib/dockerfile.ts
+++ lib/dockerfile.ts
@@ -59,7 +59,6 @@ export interface DockerfileActionGroup {
 }
 
 class Dockerfile {
-
 	public actionGroups: DockerfileActionGroup[] = [];
 
 	public constructor(dockerfileContents: string) {
@@ -79,7 +78,9 @@ class Dockerfile {
 			switch (entry.name.toUpperCase()) {
 				case 'ADD':
 					// This isn't supported, as adding urls etc can really mess things up.
-					throw new UnsupportedError('Dockerfiles containing the ADD instruction are not supported. Please use COPY.');
+					throw new UnsupportedError(
+						'Dockerfiles containing the ADD instruction are not supported. Please use COPY.',
+					);
 				case 'COPY':
 					// If there are any commands which have occured already, save them as an action
 					// group
@@ -136,17 +137,16 @@ class Dockerfile {
 	 * @param files A list of files whose contents have changed. The path
 	 * should be relative to the build context
 	 */
-	public getActionGroupsFromChangedFiles(files: string[]): DockerfileActionGroup[] {
+	public getActionGroupsFromChangedFiles(
+		files: string[],
+	): DockerfileActionGroup[] {
 		let actionGroupIdx = 0;
 		for (const actionGroup of this.actionGroups) {
 			// FIXME: COPY src/ src/ won't work due to the fact that there isn't a glob on the end of
 			// the target - we can fix this by silenty converting src/ to src/*
 
 			// For every dependency, see if it matches the current action group
-			const matches = _(actionGroup.fileDependencies)
-				.flatMap(({ localPath }) => minimatch.match(files, localPath))
-				.uniq()
-				.value();
+			const matches = this.fileMatchesForActionGroup(files, actionGroup);
 
 			// If any of the files have changed we need to return all
 			// action groups which follow, as they could depend on either
@@ -160,6 +160,16 @@ class Dockerfile {
 		}
 
 		return [];
+	}
+
+	public fileMatchesForActionGroup(
+		files: string[],
+		actionGroup: DockerfileActionGroup,
+	): string[] {
+		return _(actionGroup.fileDependencies)
+			.flatMap(({ localPath }) => minimatch.match(files, localPath))
+			.uniq()
+			.value();
 	}
 
 	private static copyArgsToFileDeps(
