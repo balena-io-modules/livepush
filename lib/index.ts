1ccd5dbddf222ef5f4dc6d58066a71c53416864b
Cameron Diver <cameron@balena.io>
2018-12-31 17:17:32 +0000

    Add FSWatcher module
    Change-type: minor
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import Container, { ChangedFiles, FileUpdates } from './container';
 import Dockerfile from './dockerfile';
 import { DockerfileParseError, UnsupportedError } from './errors';
+import FSMonitor, { FSEvent } from './fs-monitor';
 
 export {
 	ChangedFiles,
@@ -8,6 +9,8 @@ export {
 	Dockerfile,
 	DockerfileParseError,
 	FileUpdates,
+	FSEvent,
+	FSMonitor,
 	UnsupportedError,
 };
 export default Container;
