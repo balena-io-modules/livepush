851c17569637277617ea1e7770e5978a985e11df
Cameron Diver <cameron@balena.io>
2020-03-20 15:57:00 +0000

    Add support for Dockerfile directives
    Because of issues in docker-file-parser:
      https://github.com/joyent/node-docker-file-parser/issues/8
    we have to first keep track of dockerfile directives, and fold
    them into the parsing performed by the external module.
    
    We also have to keep track of the escape directive, so that we can
    ensure we parse comments in the middle of commands correctly.
    
    This work leads the way to being able to specify livepush behaviour
    using dockerfile directives.
    
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@balena.io>

---
+++ test/dockerfile-parser.spec.ts
@@ -0,0 +1,115 @@
+import { expect } from 'chai';
+
+import { parseDockerfile } from '../lib/dockerfile-parser';
+
+describe('Dockerfile parsing', () => {
+	describe('Dockerfile directives', () => {
+		it('should correctly parse Dockerfile directives', () => {
+			expect(
+				parseDockerfile('#dev-cmd-live=webpack-dev-server\n'),
+			).to.deep.equal([
+				{
+					name: 'LIVECMD',
+					args: 'webpack-dev-server',
+					lineno: 1,
+					raw: 'dev-cmd-live=webpack-dev-server',
+				},
+			]);
+		});
+
+		it('should correctly parse Dockerfile directives with spaces', () => {
+			expect(
+				parseDockerfile('\n\n\n#      dev-cmd-live=webpack-dev-server\n'),
+			).to.deep.equal([
+				{
+					name: 'LIVECMD',
+					args: 'webpack-dev-server',
+					lineno: 4,
+					raw: 'dev-cmd-live=webpack-dev-server',
+				},
+			]);
+		});
+
+		it('should sort Dockerfile directives correctly with normal commands', () => {
+			expect(
+				parseDockerfile(
+					[
+						'FROM base',
+						'RUN 1',
+						'COPY 1',
+						'#dev-cmd-live=webpack-dev-server',
+						'COPY 2',
+						'RUN 2',
+						'CMD cmd',
+					].join('\n'),
+				).map(({ name }) => name),
+			).to.deep.equal(['FROM', 'RUN', 'COPY', 'LIVECMD', 'COPY', 'RUN', 'CMD']);
+
+			expect(
+				parseDockerfile(
+					[
+						'FROM base',
+						'RUN 1',
+						'COPY 1',
+						'COPY 2',
+						'RUN 2',
+						'CMD cmd',
+						'#dev-cmd-live=webpack-dev-server',
+					].join('\n'),
+				).map(({ name }) => name),
+			).to.deep.equal(['FROM', 'RUN', 'COPY', 'COPY', 'RUN', 'CMD', 'LIVECMD']);
+		});
+
+		it('should sort directives correctly even with empty lines and comments', () => {
+			expect(
+				parseDockerfile(
+					[
+						'FROM base',
+						'',
+						'RUN 1',
+						'# a comment',
+						'COPY 1',
+						'',
+						'# another comment',
+						'COPY 2',
+						'RUN 2',
+						'',
+						'',
+						'',
+						'CMD cmd',
+						'#dev-cmd-live=webpack-dev-server',
+					].join('\n'),
+				).map(({ name }) => name),
+			).to.deep.equal(['FROM', 'RUN', 'COPY', 'COPY', 'RUN', 'CMD', 'LIVECMD']);
+		});
+	});
+
+	it('should correctly parse leading spaces', () => {
+		expect(
+			parseDockerfile(
+				['FROM a', `RUN echo 'test\\`, `      test'`, 'CMD test'].join('\n'),
+			),
+		).to.deep.equal([
+			{
+				name: 'FROM',
+				lineno: 1,
+				raw: 'FROM a',
+				args: 'a',
+			},
+			{
+				name: 'RUN',
+				raw: `RUN echo 'test      test'`,
+				// This represent the line which the command
+				// *finishes* on
+				lineno: 3,
+				args: `echo 'test      test'`,
+			},
+			{
+				name: 'CMD',
+				lineno: 4,
+				args: 'test',
+				raw: 'CMD test',
+			},
+		]);
+	});
+});
